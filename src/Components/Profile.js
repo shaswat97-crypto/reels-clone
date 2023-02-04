83d217c8646287fae76c2ee1e4a4d239182cbf9a
shaswat97-crypto <shaswat97@gmail.com>
2023-02-05 02:27:46 +0530

    profile

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,30 +2,36 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Header from './Header';
 import './profile.css'
-import Avatar from '@mui/material/Avatar';
 import { useContext } from 'react';
 import { AuthContext } from '../Context/AuthContext';
 import { database } from '../firebase';
 import { Button } from '@mui/material';
-
+import CircularProgress from '@mui/material/CircularProgress';
+import Avatar from '@mui/material/Avatar';
+import Like from './Like';
+import CommentIcon from '@mui/icons-material/Comment';
+import CommentModal from './CommentModal';
+import { Box } from '@mui/system';
+import Video from './Video';
 function Profile() {
   let { id } = useParams();
   let { user } = useContext(AuthContext);
-  // console.log(user)
   const [dbUser, setDbUser] = useState(null);
-  const [posts, setPosts] = useState(null);
+  const [postDatabase, setPostDatabase] = useState(null);
   useEffect(() => {
-    if (dbUser) {
-      let arr = [];
-      for (let i = 0; i < dbUser.postIdDatabase.length; i++) {
-        database.posts.doc(dbUser.postIdDatabase[i]).get()
-          .then((data) => {
-            arr.push(data.data());
-          })
-      }
-      setPosts(arr);
-    }
-  }, [dbUser])
+    let pArr = []
+    let unsub = database.posts.orderBy('createdAt', 'desc').onSnapshot((snapshot) => {
+      pArr = [];
+      snapshot.forEach((doc) => {
+        let data = { ...doc.data(), pId: doc.id };
+        pArr.push({ ...data });
+      })
+      setPostDatabase([...pArr])
+
+    })
+    return unsub;
+  }, [])
+
   useEffect(() => {
     database.users.doc(user.uid).onSnapshot((data) => {
       setDbUser(data.data());
@@ -47,15 +53,30 @@ function Profile() {
               </div>
             </div>
             {
-              posts ?
-              <div className="bottom">
+              <div className='bottom'>
                 {
-                  posts.map((post)=>{
-                    <video src={post.pUrl}></video>
-                  })
+                  !postDatabase
+                    ?
+                    <Box sx={{ display: 'flex' }}>
+                      <CircularProgress />
+                    </Box>
+                    :
+                    <div className='post'>
+                      {
+                        postDatabase.map((post) => (
+                          <div className='frag' key={post.id}>
+                            <Video source={post} key={post.pId}></Video>
+                            <div className="avatar" >
+                              <p className='name'>{dbUser.fullName}</p>
+                            </div>
+                            <div className="likecont"><Like user={dbUser} post={post}></Like></div>
+                            <CommentModal post={post} user={dbUser}></CommentModal>
+                          </div>
+                        ))
+                      }
+                    </div>
                 }
               </div>
-              : <div>no post</div>
             }
           </div>
         </div>
